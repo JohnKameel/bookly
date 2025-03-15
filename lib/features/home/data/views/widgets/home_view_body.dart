72540f9023a7c64bcd9bd58408c06a36d37687ec
JohnKameel <jookameel10@gmail.com>
2025-03-16 01:25:17 +0200

    add featured list view to home

--- lib/features/home/data/views/widgets/home_view_body.dart
+++ lib/features/home/data/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,9 +12,29 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: const [
-        CustomAppBar()
+        CustomAppBar(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0,),
+              child: CustomListViewItem(),
+            );
+          }
+      ),
+    );
+  }
+}
