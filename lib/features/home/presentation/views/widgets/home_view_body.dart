86ac69ab0983d632a68414102b06eacc7c4ad1f0
JohnKameel <jookameel10@gmail.com>
2025-03-19 01:22:09 +0200

    create app styles

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -21,7 +21,7 @@ class HomeViewBody extends StatelessWidget {
           const SizedBox(height: 45,),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
           ),
           BestSellerListViewItem(),
         ],
