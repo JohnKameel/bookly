0961fc2a1a10283dc5304de27a82a78c28ab532a
JohnKameel <jookameel10@gmail.com>
2025-03-19 02:03:14 +0200

    best seller item done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/assets.dart';
+import 'book_rating.dart';
 
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -28,42 +29,47 @@ class BestSellerListViewItem extends StatelessWidget {
             ),
           ),
           const SizedBox(width: 30,),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and the Goblet of Fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child: const Text(
+                    'Harry Potter and the Goblet of Fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle20,
+                  ),
                 ),
-              ),
 
-              const SizedBox(height: 3,),
+                const SizedBox(height: 3,),
 
-              Text(
-                'J.K. Rowling',
-                style: Styles.textStyle14,
-              ),
+                Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14,
+                ),
 
-              const SizedBox(height: 3,),
+                const SizedBox(height: 3,),
 
-              Row(
-                children: [
-                  Text(
-                    '19.99 €',
-                    style: Styles.textStyle20.copyWith(
-                      fontWeight: FontWeight.bold,
+                Row(
+                  children: [
+                    Text(
+                      '19.99 €',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                    const Spacer(),
+                    const BookRating(),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
+
