3cddedcf76a69eb7b64210a85de3e9360f1145a9
JohnKameel <jookameel10@gmail.com>
2025-03-26 13:49:18 +0200

    search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'custom_search_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -11,9 +13,33 @@ class SearchViewBody extends StatelessWidget {
       child: Column(
         children: const [
           CustomSearchTextField(),
+          SizedBox(height: 16,),
+          Text(
+              'Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(height: 16,),
+          Expanded(child: SearchResultListView()),
         ],
       ),
     );
   }
 }
 
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0),
+          child: BookListViewItem(),
+        );
+      },
+    );
+  }
+}
